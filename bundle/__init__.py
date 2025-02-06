3232bb69834a8a661959d2c1952ddee7df18d7f3
NABIL <5687nabil@gmail.com>
2025-02-06 19:20:52 +0600

    Add files via upload

--- bundle/__init__.py
+++ bundle/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template 
+from flask import Flask
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-app = Flask(__name__, template_folder='public')
+app = Flask(__name__, template_folder='public', static_folder='static', static_url_path='')
 
 def myapp():
-    app.config['SECRET_KEY'] = 'nabil'
+    app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
+    app.config['DATABASE_URI'] = os.getenv('DATABASE_URI')
 
     from .views import views
     app.register_blueprint(views)
--- bundle/views.py
+++ bundle/views.py
@@ -3,11 +3,85 @@ import requests, os
 from dotenv import load_dotenv
 load_dotenv()
 
-views = Blueprint('views', __name__, template_folder='public')
+views = Blueprint('views', __name__, template_folder='public', static_url_path='', static_folder='static')
 
 @views.route('/')
 def index():
     userip = request.headers.get('X-forwarded-for', request.remote_addr)
-    callapi = requests.get(f'https://ipinfo.io/{userip}/json?{os.getenv('token')}')
+    callapi = requests.get(f'https://ipinfo.io/{userip}/json?{os.getenv('TOKEN')}')
     ip = callapi.json()
+
+    if ip.get("status"):
+        return render_template('ratelimit.html')
+
+
+    isp = " ".join(ip["org"].split()[1:])
+
+    if any(
+        x.lower() in isp.lower() for x in [
+            "Google",
+            "Cloudflare",
+            "Amazon",
+            "Microsoft",
+            "Oracle",
+            "Hetzner",
+            "OVH",
+            "DigitalOcean",
+            "Linode",
+            "Vultr",
+            "Leaseweb",
+            "Contabo",
+            "NordVPN",
+            "ExpressVPN",
+            "Mullvad",
+            "ProtonVPN",
+            "Surfshark",
+            "Windscribe",
+            "VyprVPN",
+            "Tor",
+            "The Onion Router",
+            "Tailscale",
+            "Private Internet Access",
+            "CyberGhost",
+            "PureVPN",
+            "IPVanish",
+            "StrongVPN"
+        ]
+    ):
+        proxy = "Yes"
+    else:
+        proxy = "No"
+
+    if "." in userip:
+        ipv = "IPv4"
+    else:
+        ipv = "IPv6"
+
+    loc_x = ip["loc"].split(",")[0]
+    loc_y = ip["loc"].split(",")[1]
+
+    country_name = requests.get(f'https://restcountries.com/v3.1/alpha/{ip["country"]}')
+
+    if country_name.status_code == 200:
+        country_name = country_name.json() 
+        ip.update(
+            {
+                "country_name": country_name[0]["name"]["common"],
+                "country_official_name": country_name[0]["name"]["official"],
+                "country_flag": country_name[0]["flags"]["png"]
+            }
+        )
+    else:
+        ip.update({"country_name": "Unknown"})
+
+    ip.update(
+        {
+            "proxy": proxy,
+            "ipv": ipv,
+            "isp": isp,
+            "x": loc_x,
+            "y": loc_y
+        }
+    )
+
     return render_template('index.html', ip=ip)

5824378e0f73c121fdae1b268533f95e1b7de2aa
NABIL <5687nabil@gmail.com>
2025-02-06 19:20:13 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from bundle import myapp
 
 if __name__ == "__main__":
     server = myapp()
-    server.run(port=5000, host='0.0.0.0', debug=True)
+    server.run(port=5000, host='0.0.0.0', debug=False)
